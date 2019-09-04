083572a1e22fbedf2701239108a11d43c7ab7051
Yacoub Belaoura <yaoubbelaoura@gmail.com>
2019-09-04 17:48:00 +0100

    :bug: fix duplication

--- scripts/002_setup_sudo.sh
+++ scripts/002_setup_sudo.sh
@@ -8,7 +8,7 @@ dpkg -i $DL/bat_${BATV}_amd64.deb
 # @fixme : fix lsof /var/lib/dpkg/lock and lock-frontend
 apt install zsh fontconfig make gcc -y
 # @idea : if using vim with vimproc
-apt install make gcc -y
+# apt install make gcc -y
 # @idea : alternative is to use neovim 
 # apt install neovim
 chsh -s $(which zsh)
