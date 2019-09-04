7ec2fda81b94e5a9efb7deab51492624cbb0ddb9
Yacoub Belaoura <yaoubbelaoura@gmail.com>
2019-09-04 12:47:01 +0100

    :todo: add todo note

--- scripts/002_setup_sudo.sh
+++ scripts/002_setup_sudo.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+source ./000_env.sh
 echo "this script need to be run with sudo"
 
-# @todo uncomment :apt install zsh neovim -y
-source ./000_env.sh
+# @fixme : fix lsof /var/lib/dpkg/lock and lock-frontend
+apt install zsh neovim -y
 dpkg -i $DL/lsd_${LSDV}_amd64.deb
 dpkg -i $DL/bat_${BATV}_amd64.deb
 
