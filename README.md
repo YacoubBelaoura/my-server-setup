3acb549ae67bdb5860dc9f62e248cbc1fb0fcb00
Yacoub Belaoura <yacoubbelaoura@gmail.com>
2019-09-05 09:52:06 +0100

    :bug: temp fix for lock and lock-frontend files

--- README.md
+++ README.md
@@ -14,9 +14,11 @@
       version: 2
       ethernets:
         enp0s3:
+          wakeonlan: true
           addresses:
           - 192.168.1.100/24
           - 192.168.8.100/24
+          gateway4: 192.168.1.1
           nameservers:
             addresses:
             - 8.8.8.8
--- scripts/002_setup_sudo.sh
+++ scripts/002_setup_sudo.sh
@@ -3,11 +3,18 @@
 source ./000_env.sh
 echo "this script need to be run with sudo"
 
+# @fixme : temp fix for lsof /var/lib/dpkg/lock and lock-frontend
+killall unattended
+killall dpkg
+
 dpkg -i $DL/lsd_${LSDV}_amd64.deb
 dpkg -i $DL/bat_${BATV}_amd64.deb
-# @fixme : fix lsof /var/lib/dpkg/lock and lock-frontend
-apt install zsh fontconfig make gcc -y
+
+apt --fix-broken install
+
+apt install zsh make gcc -y
 # @idea : if using vim with vimproc
+# @idea : optional : fontconfig and xfont-utils for spacevim 
 # apt install make gcc -y
 # @idea : alternative is to use neovim 
 # apt install neovim
